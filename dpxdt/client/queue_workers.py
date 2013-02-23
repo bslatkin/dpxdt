f1085aaa68e84fa4590c292ad8e195b8e1736ee2
Brett Slatkin <brett@haxor.com>
2013-02-23 13:22:55 -0800

    Adding heartbeat messages to queue workers; fleshed out capture worker

--- dpxdt/client/queue_workers.py
+++ dpxdt/client/queue_workers.py
@@ -22,6 +22,7 @@ import gflags
 FLAGS = gflags.FLAGS
 
 # Local modules
+import capture_worker
 import pdiff_worker
 import release_worker
 import workers
@@ -31,28 +32,66 @@ gflags.DEFINE_string(
     'pdiff_queue_url', None,
     'URL of remote perceptual diff work queue to process.')
 
+gflags.DEFINE_string(
+    'capture_queue_url', None,
+    'URL of remote webpage capture work queue to process.')
+
+
+class Error(Exception):
+    """Base-class for exceptions in this module."""
+
+class HeartbeatError(Error):
+    """Reporting the status of a task in progress failed for some reason."""
+
+class PdiffFailedError(Error):
+    """Running a perceptual diff failed for some reason."""
+
+class CaptureFailedError(Error):
+    """Capturing a webpage screenshot failed for some reason."""
+
+
+
+# TODO: Split this out into a separate FetchItem thread so we don't gum-up
+# the important workflows with messages that aren't critical.
+
+class HeartbeatWorkflow(workers.WorkflowItem):
+    """TODO"""
+
+    def run(self, queue_heartbeat_url, task_id, message, index):
+        call = yield workers.FetchItem(
+            queue_url + '/heartbeat',
+            post={
+                'task_id': task_id,
+                'message': message,
+                'index': index,
+            })
+
+        if call.json and call.json.get('error'):
+            raise HeartbeatError(call.json.get('error'))
+
+        if not call.json or not call.json.get('success'):
+            raise HeartbeatError('Bad response: %r' % call)
+
 
 class RemoteQueueWorkflow(WorkflowItem):
     """Runs a local workflow based on work items in a remote queue.
 
     Args:
-        queue_lease_url: URL to POST to for leasing new tasks.
-        queue_finish_url: URL to POST to for finishing an existing task.
+        queue_url: Base URL of the work queue.
         local_queue_workflow: WorkflowItem sub-class to create using parameters
             from the remote work payload that will execute the task.
         poll_period: How often, in seconds, to poll the queue when it's
             found to be empty.
     """
 
-    def run(self, queue_lease_url, queue_finish_url, local_queue_workflow,
-            poll_period=60):
+    def run(self, queue_url, local_queue_workflow, poll_period=60):
         while True:
-            next_item = yield workers.FetchItem(queue_lease_url, post={})
+            next_item = yield workers.FetchItem(queue_url + '/lease', post={})
 
             something_to_do = False
             if next_item.json and next_item.json['error']:
-                logging.error('Could not fetch work from queue_lease_url=%r. '
-                              '%s', queue_lease_url, next_item.json['error'])
+                logging.error('Could not fetch work from queue_url=%r. '
+                              '%s', queue_url, next_item.json['error'])
             elif next_item.json and next_item.json['tasks']:
                 something_to_do = True
 
@@ -65,36 +104,52 @@ class RemoteQueueWorkflow(WorkflowItem):
             task = task_list[0]
 
             task_id = task.pop('task_id')
-            logging.debug('Starting work item from queue_lease_url=%r, '
+            logging.debug('Starting work item from queue_url=%r, '
                           'task_id=%r, payload=%r, workflow=%r',
-                          queue_finish_url, task_id, task,
+                          queue_url, task_id, task,
                           local_queue_workflow)
+
+            # Define a heartbeat closure that will return a workflow for
+            # reporting status. This will auto-increment the index on each
+            # call, so only the latest update will be saved.
+            # TODO: Make this fire-and-forget.
+            index = [0]
+            def heartbeat(message):
+                next_index = index[0]
+                index[0] = next_index + 1
+                logging.debug('queue_url=%r, task_id=%r, message: %s',
+                              queue_url, task_id, message)
+                return HeartbeatWorkflow(
+                    queue_url + '/heartbeat', task_id, message, next_index)
+
+            task.update(heartbeat=heartbeat)
 
             try:
                 yield local_queue_workflow(**task)
-            except Exception:
+            except Exception, e:
                 logging.exception('Exception while processing work from '
-                                  'queue_lease_url=%r, task_id=%r',
-                                  queue_lease_url, task_id)
+                                  'queue_url=%r, task_id=%r',
+                                  queue_url, task_id)
+                yield heartbeat('Task failed. %s: %s' %
+                                (e.__class__.__name__, str(e)))
                 continue
 
             finish_item = yield workers.FetchItem(
-                queue_finish_url, post={'task_id': task_id})
+                queue_url + '/finish, post={'task_id': task_id})
             if finish_item.json and finish_item.json['error']:
                 logging.error('Could not finish work with '
-                              'queue_finish_url=%r, task_id=%r. %s',
-                              queue_finish_url, finish_item.json['error'],
-                              task_id)
+                              'queue_url=%r, task_id=%r. %s',
+                              queue_url, finish_item.json['error'], task_id)
 
-            logging.debug('Finished work item with queue_finish_url=%r, '
-                          'task_id=%r', queue_finish_url, task_id)
+            logging.debug('Finished work item with queue_url=%r, '
+                          'task_id=%r', queue_url, task_id)
 
 
 class DoPdiffQueueWorkflow(WorkflowItem):
     """TODO"""
 
     def run(self, build_id=None, release_name=None, release_number=None,
-            run_name=None, reference_url=None, run_url=None):
+            run_name=None, reference_url=None, run_url=None, heartbeat=None):
         output_path = tempfile.mkdtemp()
         try:
             ref_path = os.path.join(output_path, 'ref')
@@ -102,17 +157,22 @@ class DoPdiffQueueWorkflow(WorkflowItem):
             diff_path = os.path.join(output_path, 'diff')
             log_path = os.path.join(output_path, 'log')
 
-            ref_item, run_item = yield [
+            yield heartbeat('Fetching reference and run images')
+            yield [
                 workers.FetchItem(reference_url, result_path=ref_path)
                 workers.FetchItem(run_url, result_path=run_path)
             ]
 
+            yield heartbeat('Running perceptual diff process')
             pdiff = yield pdiff_worker.PdiffItem(
                 log_path, ref_path, run_path, diff_path)
-            if not os.path.isfile(diff_path) and pdiff.returncode != 0:
-                # TODO: Real exception
-                assert False, 'Error!'
 
+            output_exists = os.path.isfile(diff_path)
+            if not output_exists and pdiff.returncode != 0:
+                raise PdiffFailedError('output_exists=%r, returncode=%r' %
+                                       (output_exists, pdiff.returncode))
+
+            yield heartbeat('Reporting diff status to server')
             yield release_worker.ReportPdiffWorkflow(
                 build_id, release_name, release_number, run_name,
                 diff_path, log_path)
@@ -123,22 +183,46 @@ class DoPdiffQueueWorkflow(WorkflowItem):
 class DoCaptureQueueWorkflow(WorkflowItem):
     """TODO"""
 
-    def run(self, build_id, name, number, config_url):
-        # create a temp dir
-        # fetch the config
-        # run capture_worker.CaptureItem on the config
-        # run ReportRunWorkflow on the files
-        # delete temp dir
+    def run(self, build_id=None, release_name=None, release_number=None,
+            run_name=None, config_url=None, heartbeat=None):
+        output_path = tempfile.mkdtemp()
+        try:
+            image_path = os.path.join(output_path, 'capture')
+            log_path = os.path.join(output_path, 'log')
+            config_path = os.path.join(output_path, 'config')
+
+            yield heartbeat('Fetching webpage capture config')
+            yield workers.FetchItem(
+                config_url, result_path=config_path)
+
+            yield heartbeat('Running webpage capture process')
+
+            capture = yield capture_worker.CaptureItem(
+                log_path, config_path, image_path)
+            if capture.returncode != 0:
+                raise CaptureFailedError('returncode=%r' % capture.returncode)
+
+            yield heartbeat('Reporting capture status to server')
+            yield release_worker.ReportRunWorkflow(
+                build_id, release_name, release_number, run_name,
+                image_path, log_path, config_path)
+        finally:
+            shutil.rmtree(output_path, True)
 
 
 def register(coordinator):
     """Registers this module as a worker with the given coordinator."""
+    # TODO: Add a flag to support up to N parallel queue workers.
     if FLAGS.pdiff_queue_url:
-        # TODO: Add a flag to support up to N parallel queue workers.
         item = RemoteQueueWorkflow(
-            FLAGS.pdiff_queue_url = '/lease',
-            FLAGS.pdiff_queue_url = '/finish',
+            FLAGS.pdiff_queue_url,
             release_worker.DoPdiffQueueWorkflow)
         item.root = True
+        coordinator.input_queue.put(item)
 
-    coordinator.input_queue.put(item)
+    if FLAGS.capture_queue_url:
+        item = RemoteQueueWorkflow(
+            FLAGS.capture_queue_url,
+            release_worker.DoCaptureQueueWorkflow)
+        item.root = True
+        coordinator.input_queue.put(item)
