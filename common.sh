8ccc370e633d73157fb6b51d407d3ee072380c50
Brett Slatkin <brett@haxor.com>
2013-06-15 16:23:12 -0700

    Trying to make pdiff cross-compile for deployment

--- common.sh
+++ common.sh
@@ -3,10 +3,14 @@
 PYTHONPATH=./lib:$PYTHONPATH
 CAPTURE_SCRIPT=dpxdt/client/capture.js
 
-# Update these for your environment:
+# Update these for your testing environment:
 PHANTOMJS_BINARY=/Users/bslatkin/Downloads/phantomjs-1.9.0-macosx/bin/phantomjs
 PDIFF_BINARY=/Users/bslatkin/projects/dpxdt/pdiff/perceptualdiff
 
 # Where the API servers to run workers against live.
 RELEASE_SERVER_PREFIX=http://localhost:5000/api
 QUEUE_SERVER_PREFIX=http://localhost:5000/api/work_queue
+
+# Update these for your deployment environment:
+PHANTOMJS_DEPLOY_BINARY=/Users/bslatkin/Downloads/phantomjs-1.9.0-macosx/bin/phantomjs
+PDIFF_DEPLOY_BINARY=/Users/bslatkin/projects/dpxdt/pdiff/perceptualdiff.linux
---
+++ pdiff/toolchain_linux.cmake
@@ -0,0 +1,3 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_C_FLAGS "-m32 -march=i686 -mtune=generic -msse3" CACHE STRING "" )
+SET(CMAKE_CXX_FLAGS "-m32 -march=i686 -mtune=generic -msse3" CACHE STRING "" )
