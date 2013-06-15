4b828f788ba88fe67f3b6d7368cdf0374b3bee18
Brett Slatkin <brett@haxor.com>
2013-06-15 14:41:17 -0700

    Updating TODOs

--- run_site_diff.sh
+++ run_site_diff.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# To run a very simple site diff when the local server is runnin, use:
+#
+# ./run_site_diff.sh \
+#   --upload_build_id=2 \
+#   --upload_release_name='blue' \
+#   http://localhost:5000/static/dummy_page1.html
+
 source common.sh
 
 ./dpxdt/client/site_diff.py \
