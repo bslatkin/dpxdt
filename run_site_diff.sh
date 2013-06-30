47329bac75f3d22cfbff516b144c4cbc97b42df6
Brett Slatkin <brett@haxor.com>
2013-06-29 17:57:44 -0700

    Fixing local site diff script

--- run_site_diff.sh
+++ run_site_diff.sh
@@ -3,7 +3,5 @@
 source common.sh
 
 ./dpxdt/client/site_diff.py \
-    --phantomjs_binary=$PHANTOMJS_BINARY \
-    --phantomjs_script=$CAPTURE_SCRIPT \
     --release_server_prefix=http://localhost:5000/api \
     "$@"
