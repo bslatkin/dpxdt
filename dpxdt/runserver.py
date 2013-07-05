87503784d6be8478e36221bf55940c9dc3b51aa5
Brett Slatkin <brett@haxor.com>
2013-07-05 00:29:10 -0700

    Moving all dependencies into a dependencies directory

--- dpxdt/runserver.py
+++ dpxdt/runserver.py
@@ -64,20 +64,19 @@ def main(argv):
 
     if FLAGS.local_queue_workers:
         coordinator = runworker.run_workers()
+        # If the babysitter thread dies, the whole process goes down.
+        def worker_babysitter():
+            try:
+                coordinator.wait_one()
+            finally:
+                os._exit(1)
+
+        babysitter_thread = threading.Thread(target=worker_babysitter)
+        babysitter_thread.setDaemon(True)
+        babysitter_thread.start()
 
     if FLAGS.ignore_auth:
         server.app.config['IGNORE_AUTH'] = True
-
-    # If the babysitter thread dies, the whole process goes down.
-    def worker_babysitter():
-        try:
-            coordinator.wait_one()
-        finally:
-            os._exit(1)
-
-    babysitter_thread = threading.Thread(target=worker_babysitter)
-    babysitter_thread.setDaemon(True)
-    babysitter_thread.start()
 
     server.app.run(debug=FLAGS.reload_code, port=FLAGS.port)
 
