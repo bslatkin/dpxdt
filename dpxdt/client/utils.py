ac7d76ebf7386c4098c3cf05fea6f3c141928b69
Brett Slatkin <brett@haxor.com>
2015-02-14 17:41:41 -0800

    Fixing a space

--- dpxdt/client/utils.py
+++ dpxdt/client/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2015Brett Slatkin
+# Copyright 2015 Brett Slatkin
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
